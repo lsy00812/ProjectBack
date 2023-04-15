3f44fa973f6ff2c9bdfd6111a26eb2398e3452c8
DESKTOP-QJ5VISR\jksin <jksin13@naver.com>
2023-04-15 14:09:53 +0900

    환경구성 시작

---
+++ src/main/java/LastProject/LastProjectApplication.java
@@ -0,0 +1,13 @@
+package LastProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastProjectApplication.class, args);
+	}
+
+}
